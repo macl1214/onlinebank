817918f3a3d857a2c1d76354b6d60fd5e3bf0029
Miguel Cubas Lopez <mcubas1214@gmail.com>
2022-06-17 23:23:43 -0400

    Add routing for registration page

--- src/main/java/com/cubas/onlinebank/controller/MainController.java
+++ src/main/java/com/cubas/onlinebank/controller/MainController.java
@@ -1,13 +1,31 @@
 package com.cubas.onlinebank.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cubas.onlinebank.entity.User;
+import com.cubas.onlinebank.service.UserService;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	UserService userService;
+	
 	@GetMapping("/")
-	public String getHome() {
+	public String getHome(Model model) {
+		
+		List<User> users = userService.getUsers();
+		
+		users.forEach(user -> System.out.println(user));
 		
 		return "home";
 	}
@@ -16,6 +34,20 @@ public class MainController {
 	public String getLogin() {
 		
 		return "authentication/login";
+	}
+	
+	@GetMapping("/register")
+	public String getRegistration(Model model) {
+		
+		model.addAttribute("user", new User());
+		
+		return "authentication/register";
+	}
+	
+	@PostMapping("/register")
+	public String saveUser(@ModelAttribute("user") User user) {
+		
+		return "redirect:/";
 	}
 	
 	@GetMapping("/aboutUs") 
